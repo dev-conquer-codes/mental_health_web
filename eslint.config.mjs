86a1c099c9e74dd06245f815a3b9752e14feadf6
dev-conquer-codes <rakshit2871@gmail.com>
2025-03-08 11:02:17 +0530

    eslint2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,16 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    overrides: [
-      {
-        files: ["**/*.ts", "**/*.tsx"],
-        rules: {
-          "no-unused-vars": "off", // ✅ Turn off for TypeScript files
-        },
-      },
-    ],
-  },
 ];
+
+export default eslintConfig;
